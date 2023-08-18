1d13a3ffd37b60a30c21e0452b9b62ed108b2d1f
Michal Malek <malek.michal.j@gmail.com>
2023-08-18 03:14:29 -0500

    almost done with assessment app
    I added the required features as well as designed my app. Now adding small quality of life details such as reversing the list order as well as separating the list by listID as well as search bar

--- app/src/main/java/com/example/fetchassess/CustomAdapter.java
+++ app/src/main/java/com/example/fetchassess/CustomAdapter.java
@@ -1,8 +1,10 @@
 package com.example.fetchassess;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,27 +15,109 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CustomAdapter{
+public class CustomAdapter extends ArrayAdapter<SomeData> {
+    private int rL;
+    private ArrayList<SomeData> dataSet;
+    Context mContext;
+
+    // View lookup cache
+    private static class ViewHolder {
+        TextView idIdent;
+        TextView listIdent;
+        TextView nameIdent;
+    }
+
+    public CustomAdapter(ArrayList<SomeData> data, Context context, int rLayout) {
+        super(context, rLayout, data);
+        this.dataSet = data;
+        this.mContext = context;
+        this.rL = rLayout;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(mContext);
+            v = vi.inflate(rL, null);
+        }
+
+        SomeData p = getItem(position);
+
+        if (p != null) {
+            TextView tt1 = (TextView) v.findViewById(R.id.identify);
+            TextView tt2 = (TextView) v.findViewById(R.id.listIdentity);
+            TextView tt3 = (TextView) v.findViewById(R.id.nameIdentity);
+
+            if (tt1 != null) {
+                tt1.setText(p.getSomeId());
+            }
+
+            if (tt2 != null) {
+                tt2.setText(p.getSomeListId());
+            }
+
+            if (tt3 != null) {
+                tt3.setText(p.getSomeName());
+            }
+        }
+
+        return v;
+    }
+
+}
+/*
+public class CustomAdapter extends RecyclerView{
     private ArrayList<SomeData> dataArrayList = new ArrayList<>();
 
-    public CustomAdapter(ArrayList<SomeData> dataArrayList) {
+    public CustomAdapter(ArrayList<SomeData> dataArrayList, Context context) {
+        super(this);
         this.dataArrayList = dataArrayList;
     }
 
-    /*
+    public void filter(ArrayList<SomeData> filterList){
+        // below line is to add our filtered
+        // list in our course array list.
+        dataArrayList = filterList;
+        // below line is to notify our adapter
+        // as change in recycler view data.
+        //notifyDataSetChanged();
+    }
+
     @NonNull
-    public CustomAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-     */
-
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    @Override
+    public CustomAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // below line is to inflate our layout.
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list, parent, false);
+        return new ViewHolder(view);
     }
 
-    public void onBindViewHolder(@NonNull CustomAdapter holder, int position) {
+    @Override
+    public void onBindViewHolder(@NonNull CustomAdapter.ViewHolder holder, int position) {
+        // setting data to our views of recycler view.
+        SomeData data = dataArrayList.get(position);
+        holder.ident.setText(data.getSomeId());
+        holder.listIdent.setText(data.getSomeListId());
+        holder.nameJ.setText(data.getSomeName());
 
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // creating variables for our views.
+        private final TextView ident;
+        private final TextView listIdent;
+        private final TextView nameJ;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // initializing our views with their ids.
+            ident = itemView.findViewById(R.id.identify);
+            listIdent = itemView.findViewById(R.id.listIdentity);
+            nameJ = itemView.findViewById(R.id.nameIdentity);
+        }
     }
 
     public int getItemCount() {return dataArrayList.size();}
 }
+ */
--- app/src/main/java/com/example/fetchassess/MainActivity.java
+++ app/src/main/java/com/example/fetchassess/MainActivity.java
@@ -1,26 +1,183 @@
 package com.example.fetchassess;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity{
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import androidx.core.splashscreen.SplashScreen;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity:";
+    private String queryCall;
+    ArrayList<SomeData> dataArrayList = new ArrayList<>();
+    ArrayList<SomeData> newArray = new ArrayList<>();
+    ArrayList<SomeData> filteredArray = new ArrayList<>();
+    ListView listRV;
+    CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Handle the splash screen.
+        SplashScreen.installSplashScreen(this);
+
         setContentView(R.layout.activity_main);
 
-        //getSupportActionBar().hide();
+        //initializing adapter class for recycler view
+        listRV = (ListView) findViewById(R.id.itemListView);
+        adapter = new CustomAdapter(dataArrayList, this, R.layout.list);
+        listRV.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
 
-        new Handler().postDelayed(new Runnable(){
+        //setting adapter class for recycler view
+        getData();
+
+
+        //Collections.reverse(dataArrayList);
+    }
+    public void getData() {
+        //query data from
+        queryCall = "https://fetch-hiring.s3.amazonaws.com/hiring.json";
+        RequestQueue requested = Volley.newRequestQueue(MainActivity.this);
+
+        //see if I get here at least, ok I do now to get to the next level
+        Log.d(TAG, "queryData");
+
+        //JSONArray Query
+        JsonArrayRequest jsonAr = new JsonArrayRequest(Request.Method.GET, queryCall, null, new Response.Listener<JSONArray>() {
+            public void onResponse(JSONArray response) {
+                //dataArrayList.clear();
+
+                try {
+                    //strings we need to get from JSON file
+                    String id;
+                    String listId;
+                    String name;
+
+                    //get JSON array from internet
+                    JSONArray allData = response;
+
+                    //length of JSONArray
+                    int length = allData.length();
+
+                    //test to see if we get data from JSON
+                    String test = allData.getJSONObject(0).getString("id");
+                    Log.d(TAG, test);
+
+                    //add from JSON file to our Java Class
+                    for (int i = 0; i < length; i++) {
+                        //variables to add to data class
+                        id = allData.getJSONObject(i).getString("id");
+                        listId = allData.getJSONObject(i).getString("listId");
+                        name = allData.getJSONObject(i).getString("name");
+
+                        //if empty, just TAG invalid, else add to array
+                        if (name.equals("") || name.equals("null")) {
+                            Log.d(TAG, "invalid");
+                        } else {
+                            dataArrayList.add(new SomeData(id, listId, name));
+                            Log.d(TAG, id);
+                        }
+                    }
+
+                    filter();
+
+                    listRV.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+                }
+                // catch any JSON errors
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
             @Override
-            public void run(){
-                startActivity(new Intent(MainActivity.this, HomeActivity.class));
-                finish();
-             }
-            }, 2000);
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requested.add(jsonAr);
+    }
+    public void filter(){
+        int l = this.dataArrayList.size();
+        int max = 0;
+
+        //find max value of listID
+        for (int i = 0; i < l; i++) {
+            String lNum = this.dataArrayList.get(i).getSomeListId();
+            if (max < Integer.valueOf(lNum)) {
+                max = Integer.valueOf(lNum);
+            }
         }
+
+        //check if we getting max value from listID
+        Log.d("Max Value", String.valueOf(max));
+
+        //Lists results by listID num
+        Collections.sort(this.dataArrayList, new Comparator<SomeData>() {
+            public int compare(SomeData lt, SomeData rt) {
+                return lt.someListId.compareTo(rt.someListId);
+            }
+        });
+
+        //split up array and added to filtered array
+        for (int i = 1; i <= max; i++) {
+            for (int j = 0; j < l; j++) {
+                if (i == Integer.valueOf(dataArrayList.get(j).getSomeListId())) {
+                    String f = dataArrayList.get(j).getSomeId();
+                    String s = dataArrayList.get(j).getSomeListId();
+                    String t = dataArrayList.get(j).getSomeName();
+                    String all = f + " " + s + " " + t;
+                    Log.d(TAG, all);
+                    newArray.add(new SomeData(f, s, t));
+                }
+            }
+
+            //sort by id/name
+            Collections.sort(newArray, new Comparator<SomeData>() {
+                public int compare(SomeData lt, SomeData rt) {
+                    return Integer.valueOf(lt.getSomeId()).compareTo(Integer.valueOf(rt.getSomeId()));
+                }
+            });
+
+            //add sectioned out array into a new filter list
+            filteredArray.addAll(newArray);
+            newArray.clear();
+        }
+
+        //clear original array and replace it with filtered list
+        dataArrayList.clear();
+        dataArrayList.addAll(filteredArray);
+    }
+
+    public void seperatedLists(){
+        int l = this.dataArrayList.size();
+        int max = 0;
+
+        //find max value of listID
+        for (int i = 0; i < l; i++) {
+            String lNum = this.dataArrayList.get(i).getSomeListId();
+            if (max < Integer.valueOf(lNum)) {
+                max = Integer.valueOf(lNum);
+            }
+        }
+
+
+    }
 }
